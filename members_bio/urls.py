a229c771d55bf4f6f70236f39f39bb610cf0e7bb
Sharatn7 <sarathn08@gmail.com>
2022-12-13 18:16:38 +0530

    Update urls.py

--- members_bio/urls.py
+++ members_bio/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('home/', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('register/addrecord/', views.addrecord, name='addrecord'),
